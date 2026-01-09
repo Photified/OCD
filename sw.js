b9e4557bb4454443e39a080b5a5d4d69b28a0b63
Photified <justinmainphoto@gmail.com>
2026-01-09 01:08:18 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,16 +3,15 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(clients.claim()); // Take control of the page immediately
 });
 
-// Listener for clicking the notification
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
     clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
       if (clientList.length > 0) return clientList[0].focus();
-      return clients.openWindow('/');
+      return clients.openWindow('./');
     })
   );
 });
